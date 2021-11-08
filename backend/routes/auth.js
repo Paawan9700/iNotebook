b8cda4a697beaf87e42b0110bada1535bd55bb70
Paawan Singal <paawansingal09@gmail.com>
2021-11-08 11:40:27 +0530

    login endpoint and sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -69,9 +69,55 @@ router.post(
 
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("some error occured");
+      res.status(500).send("internal server error");
     }
   }
 );
+
+
+// verifying a user through login (basically login route)
+router.post('/login',   [
+  body("email", "enter a valid email").isEmail(),
+  body("password", "password cannot be blank").exists()
+], async (req, res) => {
+
+    // if there are errors then return the BAD request and the errors
+    // generall errors would not be there assuming user is not mad
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+
+      if(!user){
+        return res.status(400).json({error : "please login with correct credentials"});
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+
+      if(!passwordCompare){
+        return res.status(400).json({error : "please login with correct credentials"});
+      }
+
+      const data = {
+        user : {
+          id : user.id
+        }
+      }
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({authToken})
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("internal server error");
+    }
+
+})
+
 
 module.exports = router;
