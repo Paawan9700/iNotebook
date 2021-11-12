9e323bb360b7e20dc49cf69f3c7af035b443c0a1
Paawan Singal <paawansingal09@gmail.com>
2021-11-12 17:34:15 +0530

    adding noteitem in a seperate note component

---
+++ src/components/Notes.js
@@ -0,0 +1,18 @@
+import React, {useContext} from "react";
+import notecontext from "../context/notes/notecontext";
+import Noteitem from "./Noteitem";
+
+
+export const Notes = () => {
+    const context = useContext(notecontext);
+    const {initialnotes, setnotes} = context;
+
+  return (
+    <div className="row my-4">
+      <h1> Your Notes</h1>
+      {initialnotes.map((note) => {
+        return <Noteitem note = {note}/>;
+      })}
+    </div>
+  );
+};
--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -1,13 +1,41 @@
+import React , {useState} from 'react'
 import Notecontext from "./notecontext";
 
 const NoteState = (props) => {
-    const state = {
-        name : "paawan", 
-        age : 21
+   const initialnotes = [
+    {
+      "_id": "618e4b8f276d21a344ed49c0",
+      "user": "6188a98b62a0a708caa74bf1",
+      "title": "my channel",
+      "description": "welcome to my channel",
+      "tag": "You Tube",
+      "Date": "2021-11-12T11:10:07.483Z",
+      "__v": 0
+    },
+    {
+      "_id": "618e4b97276d21a344ed49c2",
+      "user": "6188a98b62a0a708caa74bf1",
+      "title": "my channel 2",
+      "description": "welcome to my channel 2",
+      "tag": "You Tube 2",
+      "Date": "2021-11-12T11:10:15.635Z",
+      "__v": 0
+    },
+    {
+      "_id": "618e4b9f276d21a344ed49c4",
+      "user": "6188a98b62a0a708caa74bf1",
+      "title": "my channel 3",
+      "description": "welcome to my channel 3",
+      "tag": "You Tube 3",
+      "Date": "2021-11-12T11:10:23.544Z",
+      "__v": 0
     }
+  ]
+
+  const [notes, setnotes] = useState(initialnotes)
 
     return (
-        <Notecontext.Provider value = {state}>
+        <Notecontext.Provider value = {{initialnotes, setnotes}}>
             {props.children}
         </Notecontext.Provider>
     )
